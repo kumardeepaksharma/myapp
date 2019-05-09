eb80ea7ff60394cb4f591b831e3e287641781fe3
lenovo pc <lenovo pc@192.168.1.5>
2019-05-09 16:51:29 +0530

    task 400 hahu

--- src/com/app/test/Myapp.java
+++ src/com/app/test/Myapp.java
@@ -2,11 +2,22 @@ package com.app.test;
 
 public class Myapp {
 
-	public static void main(String[] args) {
-		int a=10;
-		int b=20;
-		System.out.println(a+b);
-		System.out.println("monu");
+	
+		private int a;
+		private int b;
+		public int getA() {
+			return a;
+		}
+		public void setA(int a) {
+			this.a = a;
+		}
+		public int getB() {
+			return b;
+		}
+		public void setB(int b) {
+			this.b = b;
+		}	
 		
-	}
+		
+
 }
